872374c8dbeac87392fbfbe972652a292951da60
Andrew Regan <a.jamesregan@gmail.com>
2016-05-29 07:58:03 -0400

    Add additional tests to ci

--- ci/docker_install.sh
+++ ci/docker_install.sh
@@ -9,10 +9,6 @@ set -xe
 apt-get update -yqq
 apt-get install git -yqq
 
-# install phpunit, the tool used for testing
-#curl -o /usr/local/bin/phpunit https://phar.phpunit.de/phpunit.phar
-#chmod +x /usr/local/bin/phpunit
-
 # install php dependencies
 apt-get install php7.0-mysql -yqq
 
